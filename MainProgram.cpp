a61601e5091ca36940664710711b7f6b799f90ca
EFE CİRİK <efe.cirik@bahcesehir.edu.tr>
2026-03-10 14:47:11 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -13,7 +13,7 @@ PART 1 — Variables and Arithmetic
 int addNumbers(int a, int b)
 {
     // TODO
-    // Return the sum of a and b
+ return a+b ;   // Return the sum of a and b
 
 }
 
@@ -27,9 +27,14 @@ PART 2 — Loops
 
 int sumUpToN(int n)
 {
-    // TODO
+  // TODO
     // Using a loop calculate the sum
-    // 1 + 2 + 3 + ... + n
+    // 1 + 2 + 3 + ... + nint 
+    int sum = 0;
+    for (int i =1 ; i<=n ; i++){
+    sum += i;
+    }
+    return sum;
 }
 
 
@@ -44,8 +49,15 @@ int findMaximum(vector<int> numbers)
 {
     // TODO
     // Return the largest number in the vector
+    int maxValue = numbers [0];
+    for(int i=1; i< numbers.size(); i++){
+    if (numbers[i] > maxValue){
+    maxValue = numbers[i];
+    }
+    
 }
-
+return maxValue;
+}
 
 
 /*
@@ -54,10 +66,16 @@ PART 4 — Strings
 ---------------------------------------
 */
 
-string reverseString(string text)
-{
+string reverseString(string text){
+
     // TODO
     // Return the reversed version of the string
+    string reversed = "";
+    for(int i = text.length() - 1 ; i>=0; i--)
+    {
+        reversed += text[i];
+    }
+    return reversed;
 }
 
 
@@ -79,17 +97,22 @@ public:
     // TODO
     // Create a constructor that receives
     // name and grade
-
+Student(string n , int g ){
+    name =n;
+    grade=g;
+    
+}
 
     // TODO
     // Create a function printInfo()
     // that prints:
     // Name: <name>
     // Grade: <grade>
-
+void printInfo(){
+    cout<<"Name;"<<name<<endl;
+    cout<<"Grade;"<<grade<<endl;
+}
 };
-
-
 
 /*
 ---------------------------------------
@@ -149,3 +172,4 @@ int main()
 
     return 0;
 }
+
